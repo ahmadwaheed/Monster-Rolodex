b4fffe2f35f7a556a2432a1ecdba6eaa0d003d0d
Ahmad Waheed <ahmadwaheedsalimi@gmail.com>
2020-03-01 23:47:26 -0800

    Updates

---
+++ precache-manifest.2d0c6ad36ce264f59f068b62ce2fe1e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc0df48f11c10f54f584a60faca2121f",
+    "url": "/Monster-Rolodex/index.html"
+  },
+  {
+    "revision": "373bb6e9ba0c596c139d",
+    "url": "/Monster-Rolodex/static/css/main.10cdfd5b.chunk.css"
+  },
+  {
+    "revision": "7ac67a5695528ee1b5ce",
+    "url": "/Monster-Rolodex/static/js/2.2b6dd0fc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Monster-Rolodex/static/js/2.2b6dd0fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373bb6e9ba0c596c139d",
+    "url": "/Monster-Rolodex/static/js/main.f738051d.chunk.js"
+  },
+  {
+    "revision": "f725134364d0f0838ea5",
+    "url": "/Monster-Rolodex/static/js/runtime-main.1b252e29.js"
+  }
+]);
